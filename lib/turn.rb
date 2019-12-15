f9dd4b84504736755fc5dc464d3b03906f263d82
Yueh Han <yuehwhan@gmail.com>
2019-12-15 02:13:09 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,19 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+#coverts a user's place on the board to the index integer
+def input_to_index(user_input)
+  index = user_input.to_i
+  index -= 1
+  return index
+end
+
+#checks the number to see if 1) the position is already taken and 2)it is an actual number
 def valid_move?(board, index)
   def position_taken?(array, ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
@@ -20,6 +36,23 @@ def valid_move?(board, index)
   else
     return false
   end
+end
 
+#Takes a users num and the board, and places the X or O char in that position
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
 
+#Asks user for a number, check if it is valid, and if it's not, recursively continues to ask for a number
+def turn (board)
+  puts "Please enter 1-9:"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
